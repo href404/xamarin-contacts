f08d19bb8cb692c7bec6f31b22e6ff4f31821e4f
Pierre Julien <pierrejulienpro@outlook.fr>
2020-04-17 08:58:03 +0200

    Ajout d'une API au projet permettant la récupération des contacts via des requêtes HTTP

---
+++ Contacts.API/Data/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace Contacts.API.Data
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
---
+++ Contacts.API/Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.API.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+           : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
